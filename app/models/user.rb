08b86852cacf84faba7cd5f219e44719264ca03f
Bleu-Yves-Sopoude <bleuyves@gmail.com>
2024-05-31 10:47:51 +0000

    Create task,Update task, delete task, get list of task by status Endpoints

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :task
   include Devise::JWT::RevocationStrategies::JTIMatcher
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers:{
+  devise_for :users, controllers: {
     sessions: 'users/sessions',
-    registrations:'users/registrations'
+    registrations: 'users/registrations'
   }
 
+  resources :task do
+    collection do
+      get 'by_status', to: 'task#tasks_by_status'
+    end
+  end
 end
